ed06c2033994527760d60c5cd348888135f7bed1
iroczkibela <90623225+iroczkibela@users.noreply.github.com>
2025-01-09 08:01:14 +0100

    Delete src directory

--- src/Calorie.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Calorie() {
-  return (
-    <div>
-      Cal
-    </div>
-  )
-}
--- src/Diet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Diet() {
-  return (
-    <div>
-      diet
-    </div>
-  )
-}
