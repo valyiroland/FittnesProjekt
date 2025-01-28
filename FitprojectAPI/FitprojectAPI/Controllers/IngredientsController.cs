638abcd69cfe0b5de6ff860b94c7ecb6db868df7
Dávid Radácsi <radacsid@kkszki.hu>
2025-01-28 19:24:08 +0100

    Kategória id alapján való lekérdezése

--- FitprojectAPI/FitprojectAPI/Controllers/IngredientsController.cs
+++ FitprojectAPI/FitprojectAPI/Controllers/IngredientsController.cs
@@ -8,121 +8,15 @@ namespace FitprojectAPI.Controllers
     [ApiController]
     public class IngredientsController : ControllerBase
     {
-        [HttpGet("Vegetables")]
-
-        public IActionResult GetV()
+        [HttpGet("category/{categoryId}")]
+        public IActionResult GetByCategory(int categoryId)
         {
             using (var context = new FitprojectContext())
             {
                 try
                 {
-                    var vegetables = context.FitprojectIngredients.Where(x=>x.CategoryId==1).Select(x => x.Name).ToList();
-                    return Ok(vegetables);
-                }
-                catch (Exception ex)
-                {
-
-                    return BadRequest(ex.Message);
-                }
-            }
-        }
-        [HttpGet("Fruit")]
-
-        public IActionResult GetF()
-        {
-            using (var context = new FitprojectContext())
-            {
-                try
-                {
-                    var fruit = context.FitprojectIngredients.Where(x => x.CategoryId == 2).Select(x => x.Name).ToList();
-                    return Ok(fruit);
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-            }
-        }
-        [HttpGet("Meats&Fishes")]
-
-        public IActionResult GetMF()
-        {
-            using (var context = new FitprojectContext())
-            {
-                try
-                {
-                    var meats = context.FitprojectIngredients.Where(x => x.CategoryId == 3).Select(x => x.Name).ToList();
-                    return Ok(meats);
-                }
-                catch (Exception ex)
-                {
-
-                    return BadRequest(ex.Message);
-                }
-            }
-        }
-        [HttpGet("Pasta")]
-
-        public IActionResult GetP()
-        {
-            using (var context = new FitprojectContext())
-            {
-                try
-                {
-                    var pasta = context.FitprojectIngredients.Where(x => x.CategoryId == 4).Select(x => x.Name).ToList();
-                    return Ok(pasta);
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-            }
-        }
-        [HttpGet("Nuts&Legumes")]
-
-        public IActionResult GetNL()
-        {
-            using (var context = new FitprojectContext())
-            {
-                try
-                {
-                    var nuts = context.FitprojectIngredients.Where(x => x.CategoryId == 5).Select(x => x.Name).ToList();
-                    return Ok(nuts);
-                }
-                catch (Exception ex)
-                {
-
-                    return BadRequest(ex.Message);
-                }
-            }
-        }
-        [HttpGet("Dairy")]
-
-        public IActionResult GetD()
-        {
-            using (var context = new FitprojectContext())
-            {
-                try
-                {
-                    var dairy = context.FitprojectIngredients.Where(x => x.CategoryId == 6).Select(x => x.Name).ToList();
-                    return Ok(dairy);
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-            }
-        }
-        [HttpGet("Others")]
-
-        public IActionResult GetO()
-        {
-            using (var context = new FitprojectContext())
-            {
-                try
-                {
-                    var others = context.FitprojectIngredients.Where(x => x.CategoryId == 7).Select(x => x.Name).ToList();
-                    return Ok(others);
+                    var ingredients = context.FitprojectIngredients.Where(x => x.CategoryId == categoryId).Select(x => new {x.Name, x.CalPer100g, x.Description }).ToList();
+                    return Ok(ingredients);
                 }
                 catch (Exception ex)
                 {
