3e0dd63bd116a4325b0e49cf54e30f604d4daca2
iroczkibela <iroczkib@kkszki.hu>
2025-01-30 15:23:42 +0100

    program.cs változtatások

--- FitprojectAPI/FitprojectAPI/Program.cs
+++ FitprojectAPI/FitprojectAPI/Program.cs
@@ -3,6 +3,51 @@ namespace FitprojectAPI
 {
     public class Program
     {
+        public static int SaltLength = 64;
+
+        public static string GenerateSalt()
+        {
+            Random random = new Random();
+            string karakterek = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string salt = "";
+            for (int i = 0; i < SaltLength; i++)
+            {
+                salt += karakterek[random.Next(karakterek.Length)];
+            }
+            return salt;
+        }
+
+
+
+        public static string CreateSHA256(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create()) 
+            {
+                byte[] data = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+
+        public static Dictionary<string, User> LoggedInUsers = new Dictionary<string, User>();
+
+        public static async Task SendEmail(string mailAddressTo, string subject, string body)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("coder.media1@gmail.com");
+            mail.To.Add(mailAddressTo);
+            mail.Subject = subject;
+            mail.Body = body;
+            smtpClient.Port = 587;
+            smtpClient.Credentials = new System.Net.NetworkCredential("coder.media1@gmail.com", "qmfonxwxejrysfnb");
+            smtpClient.EnableSsl = true;
+            await smtpClient.SendMailAsync(mail);
+        }
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
