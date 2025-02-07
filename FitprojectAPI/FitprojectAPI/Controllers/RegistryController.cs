1654bad2168f992fbd86d1a6e3235f104e6a7cac
valyiroland <valyir@kkszki.hu>
2025-02-07 12:06:24 +0100

    Regisztráció Aktiválása

--- FitprojectAPI/FitprojectAPI/Controllers/RegistryController.cs
+++ FitprojectAPI/FitprojectAPI/Controllers/RegistryController.cs
@@ -15,11 +15,11 @@ namespace FitprojectAPI.Controllers
             {
                 try
                 {
-                    if(context.FitprojectUsers.FirstOrDefault(u => u.Name == user.Name) != null)
+                    if (context.FitprojectUsers.FirstOrDefault(u => u.Name == user.Name) != null)
                     {
                         return BadRequest("A felhasználónév foglalt!");
                     }
-                    if(context.FitprojectUsers.FirstOrDefault(u => u.Email == user.Email) != null)
+                    if (context.FitprojectUsers.FirstOrDefault(u => u.Email == user.Email) != null)
                     {
                         return BadRequest("Az email cím már foglalt!");
                     }
@@ -28,7 +28,7 @@ namespace FitprojectAPI.Controllers
                     user.Hash = Program.CreateSHA256(user.Hash);
                     await context.FitprojectUsers.AddAsync(user);
                     await context.SaveChangesAsync();
-                    Program.SendEmail(user.Email, "Regisztráció", $"A következő linkre kattintva véglegesítse a regisztrációját: \nhttp://localhost:5000/api/Registry?felhasznaloNev={user.Name}&email={user.Email}");
+                    Program.SendEmail(user.Email, "Regisztráció", $"A következő linkre kattintva véglegesítse a regisztrációját: \nhttp://localhost:5071/Registry?name={user.Name}&email={user.Email}");
                     return Ok("Sikeres regisztráció! Az aktiváláshoz ellenőrizze az email fiókját!");
                 }
                 catch (Exception ex)
@@ -36,6 +36,39 @@ namespace FitprojectAPI.Controllers
                     return BadRequest(ex.Message);
                 }
             }
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Activate(string name, string email)
+        {
+            using (var context = new FitprojectContext())
+            {
+
+                try
+                {
+                    var user = context.FitprojectUsers.FirstOrDefault(u => u.Name == name && u.Email == email);
+                    if (user == null)
+                    {
+                        return BadRequest("Sikertelen aktiválás.");
+
+
+
+                    }
+                    user.Aktiv = 1;
+                    context.FitprojectUsers.Update(user);
+                    await context.SaveChangesAsync();
+                    return Ok("siker aktiválás!");
+
+                }
+                catch (Exception ex)
+                {
+
+                    return BadRequest(ex.Message+name+email);
+                }
+
+            }
+
         }
     }
 }
