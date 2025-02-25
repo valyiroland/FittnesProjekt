9711628c23b01907755e5749177715bf5692c3af
iroczkibela <iroczkib@kkszki.hu>
2025-02-25 19:11:43 +0100

    BmiDTO hozzáadása

---
+++ FitprojectAPI/FitprojectAPI/DTOs/BmiDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FitprojectAPI.DTOs
+{
+    public class BmiDto
+    {
+        public int UserId { get; set; }
+        public int Height { get; set; }
+        public decimal Weight { get; set; }
+        public decimal BmiValue { get; set; }
+    }
+}
