b4f8ad78e94fab40bb69d51ef6a69787774991a6
iroczkibela <iroczkib@kkszki.hu>
2025-03-10 19:37:06 +0100

    apró változtatások

--- FitprojectAPI/FitprojectAPI/Controllers/BmiController.cs
+++ FitprojectAPI/FitprojectAPI/Controllers/BmiController.cs
@@ -8,7 +8,7 @@ namespace FitprojectAPI.Controllers
     public class BmiController : ControllerBase
     {
         [HttpPost]
-        public IActionResult AddBmi([FromBody] BmiDto bmiDto)
+        public IActionResult AddOrUpdateBmi([FromBody] BmiDto bmiDto)
         {
             using (var context = new FitprojectContext())
             {
@@ -16,21 +16,35 @@ namespace FitprojectAPI.Controllers
                 {
                     if (bmiDto == null)
                     {
-                        return StatusCode(406, "Nem érkezett adat.");
+                        return StatusCode(406, "No data received.");
                     }
 
-                    FitprojectBmi newBmi = new FitprojectBmi()
-                    {
-                        UserId = bmiDto.UserId,
-                        Date = DateTime.Now,
-                        Height = bmiDto.Height,
-                        Weight = bmiDto.Weight,
-                        BmiValue = bmiDto.BmiValue
-                    };
+                    var existingBmi = context.FitprojectBmis.FirstOrDefault(b => b.UserId == bmiDto.UserId);
 
-                    context.FitprojectBmis.Add(newBmi);
+                    if (existingBmi != null)
+                    {
+                        // Ha már létezik egy rekord, frissítjük
+                        existingBmi.Date = DateTime.Now;
+                        existingBmi.Height = bmiDto.Height;
+                        existingBmi.Weight = bmiDto.Weight;
+                        existingBmi.BmiValue = bmiDto.BmiValue;
+                    }
+                    else
+                    {
+                        // Ha nincs, létrehozunk egy újat
+                        FitprojectBmi newBmi = new FitprojectBmi()
+                        {
+                            UserId = bmiDto.UserId,
+                            Date = DateTime.Now,
+                            Height = bmiDto.Height,
+                            Weight = bmiDto.Weight,
+                            BmiValue = bmiDto.BmiValue
+                        };
+                        context.FitprojectBmis.Add(newBmi);
+                    }
+
                     context.SaveChanges();
-                    return Ok("Sikeres rögzítés.");
+                    return Ok("Sikeres mentés vagy frissítés.");
                 }
                 catch (Exception ex)
                 {
@@ -38,6 +52,7 @@ namespace FitprojectAPI.Controllers
                 }
             }
         }
+
 
 
 
